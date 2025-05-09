bfe86185354888dabc59d2613a41c87e1da74dcf
rixwand <riswandir461311@gmail.com>
2025-05-09 22:08:05 +0800

    change themes to gruvbox

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,26 @@
 return {
   "nvim-lualine/lualine.nvim",
+
   config = function()
     require("lualine").setup({
       options = {
-        theme = "dracula"
-      }
+        theme = "gruvbox_dark",
+      },
     })
-  end
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "neo-tree",
+      callback = function()
+        vim.opt_local.laststatus = 0 -- hide statusline in Neo-tree
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("BufLeave", {
+      pattern = "*",
+      callback = function()
+        if vim.bo.filetype ~= "neo-tree" then
+          vim.opt.laststatus = 2 -- restore statusline elsewhere
+        end
+      end,
+    })
+  end,
 }
